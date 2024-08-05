f43518eec20074e2f353e2acc252132606decf05
fb-shaik <128546053+fb-shaik@users.noreply.github.com>
2024-08-05 11:49:45 +0200

    Add files via upload

---
+++ BookCollection_Indexer_G2/BookCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCollection_Indexer_G2
+{
+    class BookCollection
+    {
+        //Should contain a List<string> store book titles
+        private List<string> books = new List<string>();
+    
+        //Define an indexer to get/set book titles at specified indexes
+            public string this[int index]
+        {
+            get 
+            { if(index <0 || index >= books.Count)
+                { throw new IndexOutOfRangeException("Index out of range"); }
+                return books[index];
+            }
+            
+            set 
+            { if(index <0 || index >= books.Count)
+                    { throw new IndexOutOfRangeException("Index out of range"); }
+                    books[index] = value;
+            }
+        }
+
+        //AddBook method: Add a book to the collection 
+        public void AddBook(string book) 
+        { 
+            books.Add(book);
+        }
+
+
+        //Count property to get the number of books in the collection
+        public int Count
+        {
+            get { return books.Count; }
+        }
+    }
+}
---
+++ BookCollection_Indexer_G2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Program.cs
+using BookCollection_Indexer_G2;
+
+class Program {
+    static void Main(String[] args) 
+    {
+        //Create an instance of the BookCollection
+        BookCollection myBooks = new BookCollection();
+
+        //Add some books to the collection
+        myBooks.AddBook("The Great Gatsby");
+        myBooks.AddBook("1984");
+        myBooks.AddBook("To Kill a Mockingbird");
+
+        //Demo the use of the indexer (access & modifying book collection)
+        Console.WriteLine("Book title at index 1: " + myBooks[1]);
+
+        //Change the title stored at index[1] using the indexer
+        myBooks[1] = "Brave New World!";
+
+        //Access the updated title after modification via the indexer
+        Console.WriteLine("Book title at index 1 after modification: " + myBooks[1]);
+
+        //Loop through the collection & print all book titles
+        Console.WriteLine("\nAll Books in the collection:");
+        for (int i = 0; i < myBooks.Count; i++) 
+        {
+            Console.WriteLine("Book at index " + i + ": " + myBooks[i]); 
+        }
+
+    }
+
+}
