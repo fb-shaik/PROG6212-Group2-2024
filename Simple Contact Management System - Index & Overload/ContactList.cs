3b879c4dc20c504b173688d9afcf8de8e40d7896
fb-shaik <128546053+fb-shaik@users.noreply.github.com>
2024-09-02 20:45:30 +0200

    Add files via upload

---
+++ Simple Contact Management System - Index & Overload/ContactList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Contact_Management_System___Index___Overload
+{
+    public class ContactList
+    {
+        private List<Contact> contacts = new List<Contact>();
+
+        // Indexer to access contacts by index
+        public Contact this[int index]
+        {
+            get { return contacts[index]; }
+            set { contacts[index] = value; }
+        }
+
+        // Method to add a contact
+        public void Add(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        // Method to remove a contact
+        public bool Remove(Contact contact)
+        {
+            return contacts.Remove(contact);
+        }
+    }
+
+}
